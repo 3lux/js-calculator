c13e0316d51a2529e1bf9d07529668dfc8052f20
Sean <seanfindlay3@gmail.com>
2018-03-17 20:37:11 +0000

    Add basic functionality

--- main.js
+++ main.js
@@ -1,20 +1,55 @@
 const output = document.getElementById("output");
-var numString = "";
+var numString = "0";
+var value1;
+var value2;
+var operationType;
+var clearNext = false;
+
+function clearOutput(){
+	output.innerHTML = '0';
+	numString = "";
+	value1 = 0;
+	value2 = 0;
+	operationType = "";
+}
 
 function push(text){
-	numString += text;
+	if (clearNext == true){
+		clearOutput();
+		clearNext = false;
+	}
+	if (numString == "0"){
+		numString = text;
+	}else{
+		numString += text;
+	}
 	if (numString.length >= 20){
 		console.log('Too long');
 	}else{
 		output.innerHTML = numString;
 	}
 }
-
-function clear(){
-	console.log('clear');
-	output.innerHTML = '0';
-	numstring = "";
-	console.log(numString);
+function equals(){
+	console.log("Equals");
+	value2 = Number(numString);
+	switch (operationType){
+		case "add":
+			output.innerHTML = value1 + value2;
+			break;
+		case "sub":
+			output.innerHTML = value1 - value2;
+			break;
+		case "div":
+			output.innerHTML = value1 / value2;
+			break;
+		case "times":
+			output.innerHTML = value1 * value2;
+			break;
+		default:
+			output.innerHTML = numString;
+			break;
+	}
+	clearNext = true;
 }
 
 function add(a, b){
@@ -31,4 +66,32 @@ function multiply(a, b){
 
 function divide(a, b){
 	return a/b;
+}
+
+function addition(){
+	operationType = "add";
+	value1 = Number(numString);
+	console.log(value1);
+	numString = "0"
+}
+
+function minus(){
+	operationType = "sub";
+	value1 = Number(numString);
+	console.log(value1);
+	numString = "0"
+}
+
+function division(){
+	operationType = "div";
+	value1 = Number(numString);
+	console.log(value1);
+	numString = "0"
+}
+
+function times(){
+	operationType = "times";
+	value1 = Number(numString);
+	console.log(value1);
+	numString = "0"
 }
